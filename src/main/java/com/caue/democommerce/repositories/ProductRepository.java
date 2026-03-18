3e6b75f9997317832d7820d67f49a3d54bc94a1a
Cauê Ribeiro <caueribeiro.dev@gmail.com>
2026-03-18 16:16:31 -0300

    Add searchByName method to ProductRepository for product name search

--- src/main/java/com/caue/democommerce/repositories/ProductRepository.java
+++ src/main/java/com/caue/democommerce/repositories/ProductRepository.java
@@ -1,7 +1,18 @@
 package com.caue.democommerce.repositories;
 
+import com.caue.democommerce.dto.ProductDTO;
 import com.caue.democommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    @Query("SELECT new com.caue.democommerce.dto.ProductDTO(obj) FROM Product obj WHERE UPPER(obj.name) LIKE UPPER" +
+            "('%'||:name ||'%')")
+    Page<ProductDTO> searchByName(@Param("name") String name, Pageable pageable);
+
 }
